0e85429be1b41f543b0548d11ccf15961cbd4a35
dev-lovi <santiagolovisotto@gmail.com>
2024-01-29 21:08:56 -0300

    Java

--- Head_First_Java/Chapter_01/MyFirstApp.java
+++
@@ -1,8 +0,0 @@
-package Head_First_Java.Chapter_01;
-
-public class MyFirstApp {
-    public static void main(String[] args){
-        System.out.println("I rule!");
-        System.out.println("the World");
-    }
-}
